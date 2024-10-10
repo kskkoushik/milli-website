696b8655a24cee1005d98a4a4c8212b02c1efeea
kskkoushik <kskkoushik135@gmail.com>
2024-10-10 20:12:55 +0530

    updated static site

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/milli-website", // Replace with your GitHub repository name
-  assetPrefix: "/milli-website/", // Replace with your GitHub repository name
-};
-
-export default nextConfig;
